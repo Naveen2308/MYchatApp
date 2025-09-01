42af4b209caf2f62f821ed0e605b604abce1ad58
DESKTOP-BUKE5C3\DELL <naveenp@snovasys.com>
2025-09-01 13:04:15 +0530

    changes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ io.sockets.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
